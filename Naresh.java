fa776e90cc4461dbe68ef387c20cfa490acecd88
UpparaNaresh <naresh.uppara@gmail.com>
2020-06-12 16:10:31 +0530

    Update Naresh.java

--- Naresh.java
+++ Naresh.java
@@ -1,8 +1,13 @@
+import java.io.util.*;
 class Naresh
 {
 public static void main(String args[])
   {
-  int a=10,b=90,c;
+    int a,b,c;
+  System.out.println("Enter a and b values");
+  Scanner sc = new Scanner(System.in);
+  a = sc.nextInt();
+  b = sc.nextInt();
   c = a+b;
 System.out.println("Hello world:"+c);
    }
