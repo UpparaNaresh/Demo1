099df7febc9ffcdd639872494851aa0d5809dbe9
UpparaNaresh <naresh.uppara@gmail.com>
2020-12-22 22:22:26 +0530

    Update Naresh.java
    Changed the logic

--- Naresh.java
+++ Naresh.java
@@ -4,11 +4,7 @@ class Naresh
 public static void main(String args[])
   {
     int a=50,b=152,c;
-//  System.out.println("Enter a and b values");
- // Scanner sc = new Scanner(System.in);
-//  a = sc.nextInt();
-//  b = sc.nextInt();
-  c = a*b;
+c = a * b + a - b;
 System.out.println("Hello world:"+c);
    }
 }
